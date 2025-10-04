981f8ffe77970713c08583799a435140bf31fd1a
Nhlanhla-Hlongwane <nhlanhlachantelle37@gmail.com>
2025-10-04 13:40:58 +0200

    Checking if lut values are correct

--- Core/Src/generator.py
+++ Core/Src/generator.py
@@ -1,20 +1,32 @@
 import numpy as np
+import matplotlib.pyplot as plt
 
 N = 128
 max_val = 4095
 
 # Sine LUT
-sine = ( (np.sin(2*np.pi*np.arange(N)/N) + 1) / 2 * max_val ).astype(int)
+sine = ((np.sin(2 * np.pi * np.arange(N) / N) + 1) / 2 * max_val).astype(int)
 
 # Sawtooth LUT
-saw = ( np.linspace(0, max_val, N, endpoint=False) ).astype(int)
+saw = np.linspace(0, max_val, N, endpoint=False).astype(int)
 
 # Triangle LUT
 triangle = np.concatenate([
-    np.linspace(0, max_val, N//2, endpoint=False),
-    np.linspace(max_val, 0, N//2, endpoint=False)
+    np.linspace(0, max_val, N // 2, endpoint=False),
+    np.linspace(max_val, 0, N // 2, endpoint=False)
 ]).astype(int)
 
-print("Sine LUT:", sine.tolist())
-print("Sawtooth LUT:", saw.tolist())
-print("Triangle LUT:", triangle.tolist())
+# ---- Plotting ----
+plt.figure(figsize=(10, 6))
+
+plt.plot(sine, label="Sine Wave", marker='o')
+plt.plot(saw, label="Sawtooth Wave", marker='s')
+plt.plot(triangle, label="Triangle Wave", marker='^')
+
+plt.title("Waveform Lookup Tables (LUTs)")
+plt.xlabel("Sample Index")
+plt.ylabel("Amplitude (0 - 4095)")
+plt.grid(True, linestyle='--', alpha=0.6)
+plt.legend()
+plt.tight_layout()
+plt.show()
--- Core/Src/main.c
+++ Core/Src/main.c
@@ -36,7 +36,7 @@
 // TODO: Add values for below variables
 #define NS 128        // Number of samples in LUT
 #define TIM2CLK 16000000  // STM Clock frequency: Hint You might want to check the ioc file
-#define F_SIGNAL 10000	// Frequency of output analog signal
+#define F_SIGNAL 20000	// Frequency of output analog signal
 
 /* USER CODE END PD */
 
